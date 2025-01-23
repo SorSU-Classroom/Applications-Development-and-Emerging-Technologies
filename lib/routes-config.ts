07612be65a571382751713c3ca04fb634cdab10e
Jarrian Gojar <jarrianvincegojar@gmail.com>
2025-01-23 23:46:56 +0800

    feat: update next.config.ts to support additional image sources and modify routes for improved documentation structure

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -46,36 +46,14 @@ export type EachRoute = {
 
 export const ROUTES: EachRoute[] = [
 	{
-		title: 'Getting Started',
-		href: '/getting-started',
+		title: 'Introduction to AppDev and EmTech',
+		href: '/introduction-to-applications-development-and-emerging-technologies',
 		noLink: true,
 		items: [
 			{ title: 'Introduction', href: '/introduction' },
 			{
-				title: 'Installation',
-				href: '/installation',
-			},
-			{ title: 'Quick Start Guide', href: '/quick-start-guide' },
-			{
-				title: 'Project Structure',
-				href: '/project-structure',
-			},
-			{
-				title: 'Components',
-				href: '/components',
-				items: [
-					{ title: 'Stepper', href: '/stepper' },
-					{ title: 'Tabs', href: '/tabs' },
-					{ title: 'Note', href: '/note' },
-					{ title: 'Code Block', href: '/code-block' },
-					{ title: 'Image & Link', href: '/image-link' },
-					{ title: 'Custom', href: '/custom' },
-				],
-			},
-			{ title: 'Themes', href: '/themes' },
-			{
-				title: 'Customize',
-				href: '/customize',
+				title: 'Different Types of Application Development',
+				href: '/different-types-of-application-development',
 			},
 		],
 	},
--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,45 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.freepik.com/**",
-      },
-    ],
-  },
-  // if used turbopack
-  // transpilePackages: ["next-mdx-remote"],
+	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'img.freepik.com/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'images.unsplash.com/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn.pixabay.com/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'image.shutterstock.com/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'source.unsplash.com/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'static-cse.canva.com/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn.builtin.com/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 't2informatik.de/**',
+			},
+		],
+	},
+	// if used turbopack
+	// transpilePackages: ["next-mdx-remote"],
 };
 
 export default nextConfig;
