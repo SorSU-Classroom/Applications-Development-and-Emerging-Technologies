11752cd08109f7c11500ef178aa36ec5a415bbbb
Jarrian Gojar <jarrianvincegojar@gmail.com>
2025-01-25 11:59:44 +0800

    feat: refactor lesson pagination to use Button component for navigation links

--- components/lessons-pagination.tsx
+++ components/lessons-pagination.tsx
@@ -1,7 +1,7 @@
 import { getPreviousNext } from '@/lib/markdown';
 import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
 import Link from 'next/link';
-import { buttonVariants } from './ui/button';
+import { Button, buttonVariants } from './ui/button';
 
 export default function LessonPagination({ pathname }: { pathname: string }) {
 	const res = getPreviousNext(pathname);
@@ -11,34 +11,40 @@ export default function LessonPagination({ pathname }: { pathname: string }) {
 			<div>
 				{res.prev && (
 					<Link
-						className={buttonVariants({
-							variant: 'outline',
-							className: 'no-underline w-full flex flex-col pl-3 !py-8 !items-start',
-						})}
 						href={`/lessons${res.prev.href}`}
+						className='!no-underline'
 					>
-						<span className='flex items-center text-muted-foreground text-xs'>
-							<ChevronLeftIcon className='w-[1rem] h-[1rem] mr-1' />
-							Previous
-						</span>
-						<span className='mt-1 ml-1'>{res.prev.title}</span>
+						<Button
+							color='mode'
+							variant={'outline'}
+							className='border-foreground/20 w-full flex flex-col pl-3 !py-8 !items-start'
+						>
+							<span className='flex items-center text-muted-foreground text-xs'>
+								<ChevronLeftIcon className='w-[1rem] h-[1rem] mr-1' />
+								Previous
+							</span>
+							<span className='mt-1 ml-1'>{res.prev.title}</span>
+						</Button>
 					</Link>
 				)}
 			</div>
 			<div>
 				{res.next && (
 					<Link
-						className={buttonVariants({
-							variant: 'outline',
-							className: 'no-underline w-full flex flex-col pr-3 !py-8 !items-end',
-						})}
 						href={`/lessons${res.next.href}`}
+						className='!no-underline'
 					>
-						<span className='flex items-center text-muted-foreground text-xs'>
-							Next
-							<ChevronRightIcon className='w-[1rem] h-[1rem] ml-1' />
-						</span>
-						<span className='mt-1 mr-1'>{res.next.title}</span>
+						<Button
+							color='mode'
+							variant={'outline'}
+							className='border-foreground/20 w-full flex flex-col pr-3 !py-8 !items-end'
+						>
+							<span className='flex items-center text-muted-foreground text-xs'>
+								Next
+								<ChevronRightIcon className='w-[1rem] h-[1rem] ml-1' />
+							</span>
+							<span className='mt-1 mr-1'>{res.next.title}</span>
+						</Button>
 					</Link>
 				)}
 			</div>
