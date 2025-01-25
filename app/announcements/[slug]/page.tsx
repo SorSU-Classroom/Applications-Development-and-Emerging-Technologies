2cad52c7a6f51e5ebc1b9271b2b7fd532bb99a97
Jarrian Gojar <jarrianvincegojar@gmail.com>
2025-01-25 12:31:51 +0800

    feat: add course orientation announcement and refactor back link to use Button component

--- app/announcements/[slug]/page.tsx
+++ app/announcements/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { Typography } from '@/components/typography';
-import { buttonVariants } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Author, getAllAnnouncementStaticPaths, getAnnouncementForSlug } from '@/lib/markdown';
 import { ArrowLeftIcon } from 'lucide-react';
 import Link from 'next/link';
@@ -42,13 +42,15 @@ export default async function AnnouncementPage(props: PageProps) {
 	return (
 		<div className='lg:w-[60%] sm:[95%] md:[75%] mx-auto'>
 			<Link
-				className={buttonVariants({
-					variant: 'link',
-					className: '!mx-0 !px-0 mb-7 !-ml-1 ',
-				})}
 				href='/announcements'
+				className='no-underline'
 			>
-				<ArrowLeftIcon className='w-4 h-4 mr-1.5' /> Back to announcements
+				<Button
+					variant='link'
+					className='!mx-0 !px-0 mb-7 !-ml-1 '
+				>
+					<ArrowLeftIcon className='w-4 h-4 mr-1.5' /> Back to announcements
+				</Button>
 			</Link>
 			<div className='flex flex-col gap-3 pb-7 w-full mb-2'>
 				<p className='text-muted-foreground text-sm'>{formatDate(res.frontmatter.date)}</p>
