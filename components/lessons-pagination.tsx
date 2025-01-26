0410df2aff377a1a9cf83cf566fe8226654f7a53
Jarrian Gojar <jarrianvincegojar@gmail.com>
2025-01-26 17:33:41 +0800

    refactor: improve layout and styling in LessonPagination component

--- components/lessons-pagination.tsx
+++ components/lessons-pagination.tsx
@@ -1,53 +1,56 @@
-import { getPreviousNext } from '@/lib/markdown';
-import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
-import Link from 'next/link';
-import { Button } from './ui/button';
+import { getPreviousNext } from "@/lib/markdown";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { cn } from "../lib/utils";
 
 export default function LessonPagination({ pathname }: { pathname: string }) {
-	const res = getPreviousNext(pathname);
+  const res = getPreviousNext(pathname);
 
-	return (
-		<div className='grid grid-cols-2 flex-grow sm:py-10 py-7 gap-3'>
-			<div>
-				{res.prev && (
-					<Link
-						href={`/lessons${res.prev.href}`}
-						className='!no-underline'
-					>
-						<Button
-							color='mode'
-							variant={'outline'}
-							className='h-auto border-foreground/20 w-full flex flex-col pl-3 !py-8 !items-start'
-						>
-							<span className='flex items-center text-muted-foreground text-xs'>
-								<ChevronLeftIcon className='w-[1rem] h-[1rem] mr-1' />
-								Previous
-							</span>
-							<span className='mt-1 ml-1 inline text-wrap text-left'>{res.prev.title}</span>
-						</Button>
-					</Link>
-				)}
-			</div>
-			<div>
-				{res.next && (
-					<Link
-						href={`/lessons${res.next.href}`}
-						className='!no-underline'
-					>
-						<Button
-							color='mode'
-							variant={'outline'}
-							className='h-auto border-foreground/20 w-full flex flex-col pr-3 !py-8 !items-end'
-						>
-							<span className='flex items-center text-muted-foreground text-xs'>
-								Next
-								<ChevronRightIcon className='w-[1rem] h-[1rem] ml-1' />
-							</span>
-							<span className='mt-1 mr-1 inline text-wrap text-right'>{res.next.title}</span>
-						</Button>
-					</Link>
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className="grid flex-grow grid-cols-2 gap-3 py-7 sm:py-10">
+      <div>
+        {res.prev && (
+          <Link href={`/lessons${res.prev.href}`} className="!no-underline">
+            <Button
+              color="mode"
+              variant={"outline"}
+              className={cn(
+                "flex h-auto w-full flex-col !items-start border-foreground/20 !py-4 pl-3"
+              )}
+            >
+              <span className="flex items-center text-xs text-muted-foreground">
+                <ChevronLeftIcon className="mr-1 h-[1rem] w-[1rem]" />
+                Previous
+              </span>
+              <span className="ml-1 mt-1 inline text-wrap text-left">
+                {res.prev.title}
+              </span>
+            </Button>
+          </Link>
+        )}
+      </div>
+      <div>
+        {res.next && (
+          <Link href={`/lessons${res.next.href}`} className="!no-underline">
+            <Button
+              color="mode"
+              variant={"outline"}
+              className={cn(
+                "flex h-auto w-full flex-col !items-end border-foreground/20 !py-4 pr-3"
+              )}
+            >
+              <span className="flex items-center text-xs text-muted-foreground">
+                Next
+                <ChevronRightIcon className="ml-1 h-[1rem] w-[1rem]" />
+              </span>
+              <span className="mr-1 mt-1 inline text-wrap text-right">
+                {res.next.title}
+              </span>
+            </Button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 }
