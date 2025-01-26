44219d8ce2c08f0e1a8265d657962f26d5c8ade5
Jarrian Gojar <jarrianvincegojar@gmail.com>
2025-01-26 17:02:10 +0800

    feat: add Prettier configuration and update dependencies for code formatting

--- components/lessons-menu.tsx
+++ components/lessons-menu.tsx
@@ -1,29 +1,24 @@
-'use client';
+"use client";
 
-import { ROUTES } from '@/lib/routes-config';
-import SubLink from './sublink';
-import { usePathname } from 'next/navigation';
+import { ROUTES } from "@/lib/routes-config";
+import SubLink from "./sublink";
+import { usePathname } from "next/navigation";
 
 export default function LessonsMenu({ isSheet = false }) {
-	const pathname = usePathname();
-	if (!pathname.startsWith('/lessons')) return null;
+  const pathname = usePathname();
+  if (!pathname.startsWith("/lessons")) return null;
 
-	return (
-		<div className='flex flex-col gap-3.5 mt-5 pr-2 pb-6'>
-			{ROUTES.map((item, index) => {
-				const modifiedItems = {
-					...item,
-					href: `/lessons${item.href}`,
-					level: 0,
-					isSheet,
-				};
-				return (
-					<SubLink
-						key={item.title + index}
-						{...modifiedItems}
-					/>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="mt-5 flex flex-col gap-2 pb-6 pr-2">
+      {ROUTES.map((item, index) => {
+        const modifiedItems = {
+          ...item,
+          href: `/lessons${item.href}`,
+          level: 0,
+          isSheet,
+        };
+        return <SubLink key={item.title + index} {...modifiedItems} />;
+      })}
+    </div>
+  );
 }
--- components/sublink.tsx
+++ components/sublink.tsx
@@ -1,92 +1,94 @@
-import { EachRoute } from '@/lib/routes-config';
-import Anchor from './anchor';
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
-import { cn } from '@/lib/utils';
-import { SheetClose } from '@/components/ui/sheet';
-import { ChevronDown, ChevronRight } from 'lucide-react';
-import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
+import { EachRoute } from "@/lib/routes-config";
+import Anchor from "./anchor";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { cn } from "@/lib/utils";
+import { SheetClose } from "@/components/ui/sheet";
+import { ChevronDown, ChevronRight } from "lucide-react";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function SubLink({
-	title,
-	href,
-	items,
-	noLink,
-	level,
-	isSheet,
+  title,
+  href,
+  items,
+  noLink,
+  level,
+  isSheet,
 }: EachRoute & { level: number; isSheet: boolean }) {
-	const path = usePathname();
-	const [isOpen, setIsOpen] = useState(level == 0);
+  const path = usePathname();
+  const [isOpen, setIsOpen] = useState(level == 0);
 
-	useEffect(() => {
-		if (path == href || path.includes(href)) setIsOpen(true);
-	}, [href, path]);
+  useEffect(() => {
+    if (path == href || path.includes(href)) setIsOpen(true);
+  }, [href, path]);
 
-	const Comp = (
-		<Anchor
-			activeClassName='text-primary dark:font-medium font-semibold'
-			href={href}
-		>
-			{title}
-		</Anchor>
-	);
+  const Comp = (
+    <Anchor
+      activeClassName="text-primary dark:font-medium font-semibold"
+      href={href}
+    >
+      {title}
+    </Anchor>
+  );
 
-	const titleOrLink = !noLink ? (
-		isSheet ? (
-			<SheetClose asChild>{Comp}</SheetClose>
-		) : (
-			Comp
-		)
-	) : (
-		<h4 className='font-medium sm:text-sm text-primary'>{title}</h4>
-	);
+  const titleOrLink = !noLink ? (
+    isSheet ? (
+      <SheetClose asChild>{Comp}</SheetClose>
+    ) : (
+      Comp
+    )
+  ) : (
+    <h4 className="font-medium text-primary sm:text-sm">{title}</h4>
+  );
 
-	if (!items) {
-		return <div className='flex flex-col'>{titleOrLink}</div>;
-	}
+  if (!items) {
+    return (
+      <div className="flex w-full flex-col rounded-md py-1 pl-2 pr-4 hover:bg-primary/10">
+        {titleOrLink}
+      </div>
+    );
+  }
 
-	return (
-		<div className='flex flex-col gap-1 w-full'>
-			<Collapsible
-				open={isOpen}
-				onOpenChange={setIsOpen}
-			>
-				<CollapsibleTrigger className='w-full pr-5'>
-					<div className='flex items-center justify-between cursor-pointer w-full'>
-						<span className='text-left'>{titleOrLink}</span>
-						<span>
-							{!isOpen ? (
-								<ChevronRight className='h-[0.9rem] w-[0.9rem]' />
-							) : (
-								<ChevronDown className='h-[0.9rem] w-[0.9rem] text-primary' />
-							)}
-						</span>
-					</div>
-				</CollapsibleTrigger>
-				<CollapsibleContent>
-					<div
-						className={cn(
-							'flex flex-col items-start sm:text-sm dark:text-stone-300/85 text-stone-800 ml-0.5 mt-2.5 gap-3',
-							level > 0 && 'pl-4 border-l ml-1.5'
-						)}
-					>
-						{items?.map((innerLink) => {
-							const modifiedItems = {
-								...innerLink,
-								href: `${href + innerLink.href}`,
-								level: level + 1,
-								isSheet,
-							};
-							return (
-								<SubLink
-									key={modifiedItems.href}
-									{...modifiedItems}
-								/>
-							);
-						})}
-					</div>
-				</CollapsibleContent>
-			</Collapsible>
-		</div>
-	);
+  return (
+    <div className="flex w-full flex-col gap-1">
+      <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+        <CollapsibleTrigger
+          className={cn("w-full rounded-md py-1 pl-2 pr-4 hover:bg-primary/10")}
+        >
+          <div className="flex w-full cursor-pointer items-center justify-between">
+            <span className="text-left">{titleOrLink}</span>
+            <span>
+              {!isOpen ? (
+                <ChevronRight className="h-[0.9rem] w-[0.9rem]" />
+              ) : (
+                <ChevronDown className="h-[0.9rem] w-[0.9rem] text-primary" />
+              )}
+            </span>
+          </div>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <div
+            className={cn(
+              "ml-0.5 mt-2.5 flex flex-col items-start gap-2 text-stone-800 dark:text-stone-300/85 sm:text-sm",
+              level > 0 && "ml-1.5 border-l pl-4"
+            )}
+          >
+            {items?.map((innerLink) => {
+              const modifiedItems = {
+                ...innerLink,
+                href: `${href + innerLink.href}`,
+                level: level + 1,
+                isSheet,
+              };
+              return <SubLink key={modifiedItems.href} {...modifiedItems} />;
+            })}
+          </div>
+        </CollapsibleContent>
+      </Collapsible>
+    </div>
+  );
 }
